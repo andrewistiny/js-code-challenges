08567b2bca962e8b4bb3296b0f75800c79571cf7
andrewistiny <aleachman3619@myhta.org>
2018-11-14 11:41:06 -1000

    complete

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,16 @@
 module.exports = function cookieProblem(array) {
-  // write your code in here
+  var largest  = Math.max.apply(null , array);
+  var total = [];
+  console.log(
+    "**************", array, largest
+  );
+  array.forEach(function(i) {
+    var shouldAdd = largest - i;
+    console.log('shouldAdd', shouldAdd);
+    total.push(shouldAdd);  
+  });
+  let actualyTotal = total.reduce((acc, cur) =>{
+    return acc + cur;
+  });
+  return actualyTotal;
 }
