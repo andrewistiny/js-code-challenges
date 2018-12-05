d0d5525679871fa1de6b567ad06f4203913e3656
andrewistiny <aleachman3619@myhta.org>
2018-12-05 09:05:47 -1000

    needle in haystack done

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,13 @@
 module.exports = function needleInHaystack(haystack) {
-  // write code in here
+  function findNeedle (item){
+    return item == 'needle';
+  }
+  let needleIndex = haystack.findIndex(findNeedle);
+  if (needleIndex >= 0){
+    console.log('needleIndex', needleIndex);
+    return needleIndex;
+  } else if (needleIndex <= -1){
+    console.log('false');
+    return false;
+  }
 }
